8e2ff229e650a774d4003a166b20a23d31a9c635
Ranjeet3345 <yadavranjeet7770@gmail.com>
2025-05-12 15:17:00 +0530

    Episode3part5

--- App.js
+++ App.js
@@ -15,20 +15,78 @@
 // </div> 
 
 
-import React from "react";
-import ReactDOM from "react-dom/client"; // <-- Correct entry point for createRoot
+//3).. import React from "react";
+// import ReactDOM from "react-dom/client"; // <-- Correct entry point for createRoot
 
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "this is Namaste React 🚀"),
-    React.createElement("h2", {}, "this is Akshay tags"),
-  ])
+// const parent = React.createElement(
+//   "div",
+//   { id: "parent" },
+//   React.createElement("div", { id: "child" }, [
+//     React.createElement("h1", {}, "this is Namaste React 🚀"),
+//     React.createElement("h2", {}, "this is Akshay tags"),
+//   ])
+// );
+
+// console.log(parent);
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(parent);
+
+
+//4)JSX
+//res: React.createElement => Object => render=>HtmlElement
+
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// const heading=React.createElement("h1",{id:"heading"},"Namaste by React Element!!");
+// console.log(heading);
+
+// const jsxheading=<h1 id="heading" className="head" tabIndex="1">Namaste React using JSX</h1>
+
+// // JSX=>Bable transpile it to React.createElment=>React Element-JS OBject =>HTMLElement(render)
+// console.log(jsxheading);
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(jsxheading);
+
+
+//5) Component 1)Functional react component 2)Class Based Component
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+//React Element
+const heading=(
+    <h1 className="head" tabIndex="5">
+         Namaste REact using JSX For multiple lines!!
+    </h1>
 );
 
-console.log(parent);
+const Title=()=>(
+    <h1 className="head" tabIndex="5">
+         Namaste REact using JSX For multiple lines!!
+    </h1>
+);
+
+// React functional Component
+// const HeadingComponent=()=>{
+//     return (<h1 className="heading">Namaste React Functional component</h1>);
+// }
+
+const HeadingComponent=()=>(
+    <div id="container">
+        <Title/>  
+        {/* component com position */}
+    <h1 className="heading">Namaste React Functional component</h1>
+    </div>
+);
+
+
+
+
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(<HeadingComponent/>);
+
+
 
