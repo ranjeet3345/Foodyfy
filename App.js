2dbefdcf2d3c931e78e45908c12c7ac10a7d2263
Ranjeet3345 <yadavranjeet7770@gmail.com>
2025-05-13 20:30:08 +0530

    we have made the header now

--- App.js
+++ App.js
@@ -16,8 +16,36 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+
+
+
+const Header=()=>{
+    return (
+        <div className="header">
+            <div className="logo-container">
+               <img className="logo" src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png?nwm=1&nws=1&industry=All&sf=&txt_keyword=" />
+            </div>
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Contact Us</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
+    )
+};
+
+
+const Applayout=()=>{
+    return (<div className="app">
+        <Header/>
+    </div>)
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<HeadingComponent/>);
+root.render(<Applayout/>);
 
 
 
