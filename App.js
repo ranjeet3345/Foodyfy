f9e2901d70931314ed468ed17c61c58a105973bd
Ranjeet3345 <yadavranjeet7770@gmail.com>
2025-05-13 23:55:50 +0530

    Episode 4 part 1 completed

--- App.js
+++ App.js
@@ -5,6 +5,8 @@
   - Search
   - RestaurantContainer
     - RestaurantCard
+      -img
+      -name of res,star rating,cuisine,deliry tie
 * Footer
   - Copyright
   - Links
@@ -37,10 +39,57 @@ const Header=()=>{
     )
 };
 
+const styleCard={
+    backgroundColor:"#f0f0f0",
+}
+
+const RestaurantCard=()=>{
+    return (
+        <div className="res-card" style={{backgroundColor:"#f0f0f0",}}>
+            <img className="res-logo" alt="res-logo" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2024/12/20/3122e8f6-e485-4f7e-8ffb-bbcd2e3dbb6a_543141.jpg" />
+            <h3>Bikanerwala</h3>
+            <h4>Sweet,North India,</h4>
+            <h4>3.4 star</h4>
+            <h4>40-50 min</h4>
+        </div>
+    )
+};
+
+
+const Body=()=>{
+    return (
+        <div className="body">
+            <div className="search">Search</div>
+            <div className="res-container">
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                
+                                
+
+            </div>
+
+        </div>
+    )
+};
 
 const Applayout=()=>{
     return (<div className="app">
         <Header/>
+        <Body/>
     </div>)
 };
 
